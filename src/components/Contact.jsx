08a89f9442dae6381f5cdc6c8e593413958c8568
remibcn <118737122+remibcn@users.noreply.github.com>
2023-08-14 01:01:16 +0200

    first try of surge

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,29 +4,32 @@ const Contact = () => {
   return (
     <div id='contact' className='max-w-[1040] m-auto md:pl-20 p-4 py-16'>
       <h1 className='py-4 text-4xl font-bold text-center text-[#001b5e]'>Contact </h1>
-      <form action='' method='POST' encType='multipart/form-data'>
-        <div>
-            <div>
-                <label>Nom</label>
-                <input type='text' name='name'></input>
+      <form action="https://getform.io/f/0e07c9e7-7f15-4cfd-a682-59d355131cff" method='POST' encType='multipart/form-data'>
+        <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
+            <div className='flex flex-col'>
+                <label className='uppercase text-sm py-2'>Nom</label>
+                <input className='border-2 rounded-lg p-3 flex border-gray-300' type='text' name='name'></input>
             </div>
-            <div>
-                <label>Numéro de téléphone</label>
-                <input type='text' name='name'></input>
+            <div className='flex flex-col'>
+                <label className='uppercase text-sm py-2'>Numéro de téléphone</label>
+                <input className='border-2 rounded-lg p-3 flex border-gray-300'type='text' name='name'></input>
             </div>
         </div>
-        <div>
-                <label>Email</label>
-                <input type='text' name='name'></input>
+        <div className='flex flex-col py-2'>
+            <label className='uppercase text-sm py-2'>Email</label>
+            <input  className='border-2 rounded-lg p-3 flex border-gray-300'type='text' name='name'></input>
             </div>
-            <div>
-                <label>Sujet</label>
-                <input type='text' name='name'></input>
+            <div className='flex flex-col py-2'>
+                <label className='uppercase text-sm py-2'>Sujet</label>
+                <input className='border-2 rounded-lg p-3 flex border-gray-300' type='text' name='name'></input>
             </div>
-            <div>
-                <label>Message</label>
-                <textarea rows="10" type='text' name='name'></textarea>
+            <div className='flex flex-col py-2'>
+                <label className='uppercase text-sm py-2'>Message</label>
+                <textarea className='border-2 rounded-lg p-3 border-gray-300' rows="10" type='text' name='name'></textarea>
             </div>
+            <button className='bg-[#001b5e] text-gray-100 w-full p-4 rounder-lg'>
+                 Envoyer un message 
+            </button>
       </form>
     </div>
   )
--- src/components/Sidenav.jsx
+++ src/components/Sidenav.jsx
@@ -1,45 +1,45 @@
 import React, { useState } from 'react'
-import { AiOutlineMenu, AiOutlineHome, AiOutlineProject}  from 'react-icons/ai'
+import { AiOutlineMenu, AiOutlineHome, AiOutlineProject, AiOutlineContacts}  from 'react-icons/ai'
 import { GrProjects } from 'react-icons/gr'
 import { BsPerson } from 'react-icons/bs'
 
 const Sidenav = () => {
     const [nav, setNav] = useState(false);
-    const handleNave = () => {
+    const handleNav = () => {
         setNav(!nav)
     }
   return (
     <div>
-      <AiOutlineMenu onClick={handleNave} className='absolute top-4 right-4 z-[99] md:hidden'/>
+      <AiOutlineMenu onClick={handleNav} className='absolute top-4 right-4 z-[99] md:hidden'/>
       {
         nav ? (
             <div className='fixed w-full h-screen bg-white/90 flex flex-col justify-center items-center z-20'>
-                <a 
+                <a onClick={handleNav}
                 href="#main" 
                 className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 eas-in duration-200'
                 > 
                     <AiOutlineHome size={20} />
                     <span className='pl-4'>Page acceuil</span>
                 </a>
-                <a 
+                <a onClick={handleNav}
                 href="#work" 
                 className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 eas-in duration-200'
                 > 
                     <AiOutlineProject size={20} />
                     <span className='pl-4'>Work</span>
                 </a>
-                <a 
+                <a onClick={handleNav}
                 href="#projects" 
                 className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 eas-in duration-200'
                 > 
                     <GrProjects size={20} />
                     <span className='pl-4'>Projets</span>
                 </a>
-                <a 
+                <a onClick={handleNav}
                 href="#contact" 
                 className='w-[75%] flex justify-center items-center rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 eas-in duration-200'
                 > 
-                    <BsPerson size={20} />
+                    <AiOutlineContacts size={20} />
                     <span className='pl-4'>Contact</span>
                 </a>
             </div>
@@ -58,7 +58,7 @@ const Sidenav = () => {
                     <GrProjects size={20} /> 
                 </a>
                 <a href='#contact' className='rounded-full shadow-lg bg-gray-100 shadow-gray-400 m-2 p-4 cursor-pointer hover:scale-110 eas-in duration-300 '>
-                    <BsPerson size={20} /> 
+                    <AiOutlineContacts size={20} /> 
                 </a>
             </div>
         </div>
